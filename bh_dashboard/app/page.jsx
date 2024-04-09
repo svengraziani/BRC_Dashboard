fcbad6cdabcfc1ee21bf7db780050121b8427fb3
Mani <mani-k@mozark.ai>
2024-04-09 14:18:21 +0530

    project setup

---
+++ bh_dashboard/app/page.jsx
@@ -0,0 +1,13 @@
+import Footer from "@/common/Footer";
+import Header from "@/common/Header";
+import { Container, Button } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      <h1>Content</h1>
+      <Footer />
+    </Container>
+  );
+}
