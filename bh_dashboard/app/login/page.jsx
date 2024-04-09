352007506b907ea437182638b4d26bf4506bfe69
Mani <mani-k@mozark.ai>
2024-04-09 15:39:20 +0530

    added assets

--- bh_dashboard/app/login/page.jsx
+++ bh_dashboard/app/login/page.jsx
@@ -1,9 +1,12 @@
 import { Container } from "react-bootstrap";
 import "./login.scss";
+// import Image from "next/image";
+// import imageIcon from "@/Assets/images/fake.jpg";
 
 export default function Login() {
   return (
     <Container>
+      {/* <Image src={imageIcon} width={100} height={100} /> */}
       <h1>h</h1>
     </Container>
   );
