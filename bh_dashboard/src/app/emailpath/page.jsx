25cced8b374e7ea3bf6bdee384a63c7dd3b8cfe0
Sandesh <sandesh.raj@sparxitsolutions.com>
2024-04-11 17:30:49 +0530

    Pages completed

---
+++ bh_dashboard/src/app/emailpath/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Emailpath() {
+    return (
+        <section className="navigation">
+            <h2>Kunde klickt auf den Link in der E-Mail</h2>
+        </section>
+    )
+}
